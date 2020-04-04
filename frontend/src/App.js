26d7e410af545c5c445f5325b7b25ba307f5bb59
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-04-04 18:26:03 +0100

    Apollo provider client cache

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import logo from './spacex.png';
 import './App.css';
+
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: "http://localhost:4200/graphql"
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
+// const client = new ApolloClient({
+//   uri:'http://localhost:4200/graphql'
+// })
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Graphql server</h1>
+    <ApolloProvider client={client}>
+    <Router>
+    <div className="container">
+    <img src={logo} alt="Space X" style={{ width:250, display:'block', margin: 'auto', height:180  }}/>
+    <Route exact path="/" component={Launches} />
+    <Route exact path="/launch/:flight_number" component={Launch} />
     </div>
+    </Router>
+    </ApolloProvider>
   );
 }
 
