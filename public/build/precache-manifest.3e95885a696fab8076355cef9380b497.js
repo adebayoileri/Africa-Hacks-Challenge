a0b11d7c348ef5147436b2beeed9ee1c6faed770
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-04-05 13:25:42 +0100

    Made changes to graphql path

---
+++ public/build/precache-manifest.3e95885a696fab8076355cef9380b497.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562f0c08b8da39c1b96a01bbefa7ea24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc9e16eb1006a940df96",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "96ebd0b6e9532c86d847",
+    "url": "/static/js/2.eaee5bd2.chunk.js"
+  },
+  {
+    "revision": "729937898630e839994162d5d9f8ce89",
+    "url": "/static/js/2.eaee5bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc9e16eb1006a940df96",
+    "url": "/static/js/main.58c484dd.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  }
+]);
